53e2a9a1ab68864613dc79037ee4446ed70a4a79
Tablieh <waledo.1997@hotmail.com>
2022-12-13 15:35:43 +0100

    fonction de Switch

--- exo3/Tp-exo3/script.js
+++ exo3/Tp-exo3/script.js
@@ -10,47 +10,36 @@ for (let j = 1; j <= 9; j++) {   //boucle sur tout les box
   const newbox = box.cloneNode()    //clone sur la box
   board.appendChild(newbox) //on mettre la box en visible
   newbox.style.color = "white"; 
-  var vide = " "
-  var player=vide;    //player = vide
+
+  var player=O;    //player = vide
   var X="X";    // x
   var O="O";    // o
   current.innerHTML = "Current player : " + player;
+  
+       //switch sur la player
 
-      switch (player) { // switch sur la clone de la deuxieme boucle 
-        case vide:
-          newbox.addEventListener("click", function () {
+        newbox.addEventListener("click", function () {
 
-            newbox.innerText = player; // indeque la player O à la BOX
+          
+          newbox.innerText = player; // indeque la player O à la BOX
+
+          switch (player) {
+          case X :
+            player = O // switch la player sur la X
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+            break;
+
+          case O :
             player = X // switch la player sur la X
             current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
-
-          });
-        break;
-        
-        case O: // si cest player = O
-
-          newbox.addEventListener("click", function () {
-
-            newbox.innerText = player; // indeque la player O à la BOX
-            player = X // switch la player sur la X
-            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
-
-          });
-        break;
-
-        case X: // si cest player = X
-
-          newbox.addEventListener("click", function () {
-
-            newbox.innerText = player; // indeque la player X à la BOX
-            player = O // switch la player sur la O
-            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
-
-          });
-        break;
+            break;
+          
+          }
+        });
+      
 
       }
-    }
+
    
 
 
