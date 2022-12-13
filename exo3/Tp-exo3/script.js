b8e8ca9d00933ad258fd04804648212a484a7f0c
Tablieh <waledo.1997@hotmail.com>
2022-12-13 13:44:54 +0100

    exo 3 working progress

--- exo3/Tp-exo3/script.js
+++ exo3/Tp-exo3/script.js
@@ -1,57 +1,88 @@
-var TicTacToe = {
-    board: document.getElementsByClassName('board')[0],
-    fields: document.getElementsByClassName('field'),
-    currentPlayer: document.getElementById('current'),
-    checkedFields: Array(9).fill(''),
-    win: [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ],
-    init: function() {
-      this.current = 'O';
-      this.currentPlayer.textContent = this.current;
-      for (var i = 0; i < this.fields.length; i++) {
-        this.fields[i].textContent = '';
-        this.checkedFields[i] = '';
-      }
-    },
-    checkWin: function() {
-      for(var i = 0; i < this.win.length; i++) {
-        if (this.checkedFields[this.win[i][0]] === this.current && this.checkedFields[this.win[i][1]] === this.current && this.checkedFields[this.win[i][2]] === this.current) {
-          alert(this.current + ' won!');
-          this.init();
-          return true;
-        } else if (this.checkedFields.indexOf('') === -1) {
-          alert('Tie');
-          this.init();
-          return true;
-        }
-      }
-    },
-    handleClick: function(e) {
-      var currentField = e.target;
-      var currentFieldNumber = Array.prototype.indexOf.call(this.fields,currentField);
-      if (!currentField.textContent) {
-        currentField.textContent = this.current;
-        this.checkedFields[currentFieldNumber] = this.current;
-        if (!this.checkWin()) {
-          this.current = this.current === 'O' ? 'X' : 'O';
-          this.currentPlayer.textContent = this.current;
-        }
-      }
-    }
-  }
-  TicTacToe.init();
-  TicTacToe.board.addEventListener('click',function(e) {
-    TicTacToe.handleClick(e);
-  });
+const box = document.createElement("div")
+box.classList.add("box")
 
+const board = document.querySelector("#board")
+var current = document.createElement("p") // balise P qui contient la current player
+    current.innerHTML = "Current player :";
+    document.body.appendChild(current)
+    for (let j = 1; j <= 9; j++) {   //boucle sur tout les box
+      const newbox = box.cloneNode()    //clone sur la box
+      board.appendChild(newbox) //on mettre la box en visible
+      newbox.style.color = "white"; 
+      var player=" ";    //player variable qui est vide pour switch la player
+      var X="X";    // x
+      var O="O";    // o
+      switch (j) { // switch sur la clone de la deuxieme boucle 
+        case j=1: // si 1er case
+          current.innerHTML = "C'est au tour de : " + O // on modifie la balise P on mettent la curent player
+          newbox.addEventListener("click", function () {
+            newbox.innerText = O; // indeque la player O à la 1er BOX
+            player=X // switch la player
+            current.innerHTML = "C'est au tour de : " +player // on modifie la balise P on mettent la curent player
+          });
+          break;
+        case j=2:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player; // indeque la player à la 2er BOX
+            player=O // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=3:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player; // indeque la player à la 3em BOX
+            player=X // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=4:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player; // indeque la player à la 4em BOX
+            player=O // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=5:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player;  // indeque la player à la 5em BOX
+            player=X // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=6:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player;  // indeque la player à la 6em BOX
+            player=O // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=7:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player;  // indeque la player à la 7em BOX
+            player=X // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=8:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player; // indeque la player à la 8em BOX
+            player=O // switch la player
+            current.innerHTML = "C'est au tour de : " + player // on modifie la balise P on mettent la curent player
+          });
+        break;
+        case j=9:
+          newbox.addEventListener("click", function () {
+            newbox.innerText = player; // indeque la player à la 9em BOX
+            player=X // switch la player
+            var t= true; //set valeur en true pour si on a 9 sont cliquee
+            if (t == true){ // si 9 on cliquee
+              alert("Partie terminée") 
+              window.location.reload();     
+            }
+          });
+        break;
+        }
+    }
    
 
 
