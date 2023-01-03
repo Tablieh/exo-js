199e7c5c9cf18030933ae18b09c092ad5d7c29f4
tablieh <waledo.1997@hotmail.com>
2023-01-03 16:00:08 +0100

    1 to 4exo done

--- exo1/Tp-exo1/script.js
+++
@@ -1,4 +0,0 @@
-function box(x) {
-    alert ("Class : box \n -display: flex \n -width: 200px \n -height: 200px \n -justify-content: center \n -background-color: #5053ee \n -color: #f2f2f2 \n -font-size: 40px \n -font-family: 'Times New Roman' \n -text-align: center \n -line-height: 200px"); 
-
-}
